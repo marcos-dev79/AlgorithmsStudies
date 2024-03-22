2fc8af14d8729cc0343c03ec3a86036c76dcb895
Marcos M <marcosriso@hotmail.com>
2024-03-22 15:02:50 -0300

    Create FloydCycleDetectionAlgorithm.java
    Here is the solution of the algorithm

---
+++ FloydCycleDetectionAlgorithm.java
@@ -0,0 +1,47 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+        this.next = null;
+    }
+}
+
+public class FloydCycleDetectionAlgorithm {
+
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+    
+        FloydCycleDetectionAlgorithm floydCycleDetectionAlgorithm = new FloydCycleDetectionAlgorithm();
+    
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next.next; // Cycle
+    
+        boolean hasCycle = floydCycleDetectionAlgorithm.hasCycle(head);
+        System.out.println("Does the linked list have a cycle? " + hasCycle);
+    }
+}
